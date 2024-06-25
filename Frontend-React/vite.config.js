828cc44bfabd21c89b5663368a2869e552e3a65d
kartik kohapare <kartikkohapare527@gmail.com>
2024-06-26 05:06:28 +0530

    done

--- Frontend-React/vite.config.js
+++ Frontend-React/vite.config.js
@@ -3,20 +3,10 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://treading-platform.onrender.com',
-        changeOrigin: true,  // Needed for cross-origin requests
-        secure: false,       // Allow requests to an HTTPS target without HTTPS on the dev server
-        rewrite: (path) => path.replace(/^\/api/, ''),  // Rewrite the path if necessary
-      }
-    }
-  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
