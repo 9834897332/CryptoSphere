c04a155c5d048a9794b3be2575006a132645b58f
kartik kohapare <kartikkohapare527@gmail.com>
2024-06-26 02:32:27 +0530

    done

--- Backend-Spring boot/src/main/java/com/zosh/config/AppConfig.java
+++ Backend-Spring boot/src/main/java/com/zosh/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.zosh.config;
 
+
 import com.zosh.model.User;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,7 +18,6 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -25,77 +25,87 @@ import java.util.Collections;
 
 @Configuration
 public class AppConfig {
+	
+	 @Bean
+	    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(Authorize -> Authorize
-                        .requestMatchers("/api/**").authenticated()
-                        .anyRequest().permitAll()
-                )
-                .oauth2Login(oauth -> {
-                    oauth.loginPage("/login/google");
-                    oauth.authorizationEndpoint(authorization -> 
-                            authorization.baseUri("/login/oauth2/authorization"));
-                    oauth.successHandler(new AuthenticationSuccessHandler() {
+	        http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+	                .authorizeHttpRequests(Authorize -> Authorize
+//	                		.requestMatchers("/api/admin/**").hasRole("ADMIN")
+	                                .requestMatchers("/api/**").authenticated()
+	                                
+	                                .anyRequest().permitAll()
+	                )
+					.oauth2Login(oauth->{
+						oauth.loginPage("/login/google");
+						oauth.authorizationEndpoint(authorization->
+								authorization.baseUri("/login/oauth2/authorization"));
+						oauth.successHandler(new AuthenticationSuccessHandler() {
 
-                        @Override
-                        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                                            Authentication authentication) throws IOException, ServletException {
+							@Override
+							public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+																Authentication authentication) throws IOException, ServletException {
 
-                            if (authentication.getPrincipal() instanceof DefaultOAuth2User) {
-                                DefaultOAuth2User userDetails = (DefaultOAuth2User) authentication.getPrincipal();
-                                String email = userDetails.getAttribute("email");
-                                String fullName = userDetails.getAttribute("name");
-                                String phone = userDetails.getAttribute("phone");
-                                String picture = userDetails.getAttribute("picture");
-                                boolean email_verified = Boolean.TRUE.equals(userDetails.getAttribute("email_verified"));
+								if(authentication.getPrincipal() instanceof DefaultOAuth2User) {
+									DefaultOAuth2User userDetails = (DefaultOAuth2User) authentication.getPrincipal();
+									String email = userDetails.getAttribute("email");
+									String fullName=userDetails.getAttribute("name");
+									String phone=userDetails.getAttribute("phone");
+									String picture=userDetails.getAttribute("picture");
+									boolean email_verified= Boolean.TRUE.equals(userDetails.getAttribute("email_verified"));
 
-                                User user = new User();
-                                user.setVerified(email_verified);
-                                user.setFullName(fullName);
-                                user.setEmail(email);
-                                user.setMobile(phone);
-                                user.setPicture(picture);
+									User user=new User();
+									user.setVerified(email_verified);
+									user.setFullName(fullName);
+									user.setEmail(email);
+									user.setMobile(phone);
+									user.setPicture(picture);
 
-                                System.out.println("--------------- " + email +
-                                        "-------------" +
-                                        "===========" +
-                                        "-------" + user);
-                            }
-                        }
-                    });
-                })
-                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
-                .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()));  // Enable CORS
+									System.out.println("--------------- " + email+
+											"-------------"+
+											"==========="
+									+"-------"+user);
+								}
 
-        return http.build();
-    }
+							}
+						});
+					})
+	                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+	                .csrf(csrf -> csrf.disable())
+	                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+	               
+			
+			return http.build();
+			
+		}
+		
+	    // CORS Configuration
+	    private CorsConfigurationSource corsConfigurationSource() {
+	        return new CorsConfigurationSource() {
+	            @Override
+	            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+	                CorsConfiguration cfg = new CorsConfiguration();
+	                cfg.setAllowedOrigins(Arrays.asList(
+	                    "http://localhost:3000",
+	                    "http://localhost:5173",
+						"http://localhost:5174",
+	                    "http://localhost:4200",
+							"https://cryptosphere-dun.vercel.app"
+	                ));
+	                cfg.setAllowedMethods(Collections.singletonList("*"));
+	                cfg.setAllowCredentials(true);
+	                cfg.setAllowedHeaders(Collections.singletonList("*"));
+	                cfg.setExposedHeaders(Arrays.asList("Authorization"));
+	                cfg.setMaxAge(3600L);
+	                return cfg;
+	            }
+	        };
+	    }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000",
-                "http://localhost:5173",
-                "http://localhost:5174",
-                "http://localhost:4200",
-                "https://cryptosphere-dun.vercel.app"
-        ));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-        configuration.setExposedHeaders(Arrays.asList("Authorization"));
-        configuration.setAllowCredentials(true);
-        configuration.setMaxAge(3600L);
+	    @Bean
+	    PasswordEncoder passwordEncoder() {
+			return new BCryptPasswordEncoder();
+		}
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
