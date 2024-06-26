a547ba989c5ef8d4bb70b75dfc7bc67f438694a5
kartik kohapare <kartikkohapare527@gmail.com>
2024-06-27 01:43:26 +0530

    changes backend properties

--- Backend-Spring boot/src/main/java/com/zosh/config/JwtConstant.java
+++ Backend-Spring boot/src/main/java/com/zosh/config/JwtConstant.java
@@ -2,7 +2,7 @@ package com.zosh.config;
 
 public class JwtConstant {
 	
-	public static final String SECRET_KEY="wpembytrwcvnryxksdbqwjebruyGHyudqgwveytrtrCSnwifoesarjbwe";
+	public static final String SECRET_KEY="wpembytrwasdvnryxksdbqwjebruyGHyudqgwveytrtrCSnwifoesarjbwe";
 	public static final String JWT_HEADER="Authorization";
 
 }
