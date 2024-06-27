051924033275f86085c03eaeeb13c93a6cf789dc
kartik kohapare <kartikkohapare527@gmail.com>
2024-06-27 05:43:02 +0530

    cors error

--- Backend-Spring boot/src/main/java/com/zosh/controller/WithdrawalController.java
+++ Backend-Spring boot/src/main/java/com/zosh/controller/WithdrawalController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "https://cryptosphere-dun.vercel.app")
 public class WithdrawalController {
 
     @Autowired
